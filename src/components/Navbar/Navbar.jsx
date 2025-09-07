3abc6d67d8df241fadc00d32fee834bca7c4d283
Suonty2 <suontii2023@gmail.com>
2025-09-07 22:51:18 +0700

    Update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -45,11 +45,9 @@ const Navbar = () => {
       <div className="text-white py-5 flex justify-between items-center">
         {/* Logo */}
         <div className="text-lg font-semibold cursor-pointer">
-          <span className="text-[#8245ec]">&lt;</span>
-          <span className="text-white">Tarun</span>
-          <span className="text-[#8245ec]">/</span>
-          <span className="text-white">Kaushik</span>
-          <span className="text-[#8245ec]">&gt;</span>
+          <span className="text-white">Suon</span>
+          <span className="text-[#8245ec]"> </span>
+          <span className="text-white">Ty</span>
         </div>
 
         {/* Desktop Menu */}
